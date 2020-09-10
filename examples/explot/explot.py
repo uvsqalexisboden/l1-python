dd339942367ca7fde9c10e6bce2cea0c2e9b802e
Pierre COUCHENEY <pierre.coucheney@uvsq.fr>
2020-09-10 11:17:58 +0200

    test modif

--- examples/explot/explot.py
+++ examples/explot/explot.py
@@ -3,4 +3,5 @@ import numpy as np
 
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))
+# ajout commentaire
 plt.show()
--- examples/hello/hello.py
+++ examples/hello/hello.py
@@ -1,2 +1,2 @@
-msg = "hello world"
+msg = "hello monde"
 print(msg.capitalize())
